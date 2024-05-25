f53969bb5f58aeaf597696157b613bf8090bac8d
furkan-asani <asfu1011@h-ka.de>
2024-05-25 10:11:53 +0200

    Initial commit

---
+++ src/main/java/com/bosch/datasynchronization/DataSynchronizationApplication.java
@@ -0,0 +1,13 @@
+package com.bosch.datasynchronization;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DataSynchronizationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DataSynchronizationApplication.class, args);
+	}
+
+}
