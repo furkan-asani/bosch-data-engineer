c507d2cd708b11ee3dbe66667ec33c5c5ca28fad
furkan-asani <asfu1011@h-ka.de>
2024-05-26 16:41:05 +0200

    Implement hashing

---
+++ src/main/java/com/bosch/datasynchronization/Hash.java
@@ -0,0 +1,15 @@
+package com.bosch.datasynchronization;
+
+import java.nio.charset.StandardCharsets;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+import java.util.Base64;
+
+public class Hash {
+
+    public static String computeHash(String input) throws NoSuchAlgorithmException {
+        MessageDigest digest = MessageDigest.getInstance("SHA-256");
+        byte[] hash = digest.digest(input.getBytes(StandardCharsets.UTF_8));
+        return Base64.getEncoder().encodeToString(hash);
+    }
+}
